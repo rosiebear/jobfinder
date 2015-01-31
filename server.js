3c8d95be3c5591095f9ce09d9ac9129e91ce6234
emrose <emjrose@gmail.com>
2015-01-31 20:22:05 +0000

    added remote mongodb

--- server.js
+++ server.js
@@ -1,4 +1,6 @@
 var express = require('express');
+var mongoose = require('mongoose');
+var jobModel = require('./models/Job');
 
 var app = express();
 
@@ -7,8 +9,24 @@ app.set('view engine', 'jade');
 
 app.use(express.static(__dirname + '/public'));
 
+app.get('/api/jobs', function(req, res) {
+    mongoose.model('Job').find({}).exec(function(error, collection){
+        res.send(collection);
+    })
+})
+
 app.get('*', function(req, res){
     res.render('index');
 });
+
+//mongoose.connect('mongodb://localhost/jobfinder');
+mongoose.connect('mongodb://emrose:poppie@ds039431.mongolab.com:39431/jobfinder')
+
+var con = mongoose.connection;
+
+con.once('open', function(){
+    console.log('connected to mongodb successfully!')
+    jobModel.seedJobs();
+})
 
 app.listen(process.env.PORT, process.env.IP);
